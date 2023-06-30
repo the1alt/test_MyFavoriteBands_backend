68e85d3cf3bbcd0eeda91528fd88f3553e2bffd7
Tibo <t.faurel@piman-group.fr>
2023-06-30 15:33:36 +0200

    create band entity & migration

---
+++ migrations/Version20230630133009.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230630133009 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE band_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE band (id INT NOT NULL, name VARCHAR(255) NOT NULL, origin VARCHAR(255) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, start DATE DEFAULT NULL, split DATE DEFAULT NULL, founders VARCHAR(255) DEFAULT NULL, members_count INT DEFAULT NULL, style VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE band_id_seq CASCADE');
+        $this->addSql('DROP TABLE band');
+    }
+}
---
+++ src/Entity/Band.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BandRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BandRepository::class)]
+class Band
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $origin = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $city = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $start = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $split = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $founders = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $members_count = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $style = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?string
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(?string $origin): static
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(?\DateTimeInterface $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getSplit(): ?\DateTimeInterface
+    {
+        return $this->split;
+    }
+
+    public function setSplit(?\DateTimeInterface $split): static
+    {
+        $this->split = $split;
+
+        return $this;
+    }
+
+    public function getFounders(): ?string
+    {
+        return $this->founders;
+    }
+
+    public function setFounders(?string $founders): static
+    {
+        $this->founders = $founders;
+
+        return $this;
+    }
+
+    public function getMembersCount(): ?int
+    {
+        return $this->members_count;
+    }
+
+    public function setMembersCount(?int $members_count): static
+    {
+        $this->members_count = $members_count;
+
+        return $this;
+    }
+
+    public function getStyle(): ?string
+    {
+        return $this->style;
+    }
+
+    public function setStyle(?string $style): static
+    {
+        $this->style = $style;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
